eb19dd4a3d059256d9a705bbeae3f7365e5b68cc
Himanshu Vikram <htantia@Himanshus-MacBook-Pro.local>
2013-12-17 16:44:24 +0530

    fixed the email bug. fixed the smtp settings

--- config/initializers/smtp_settings.rb
+++ config/initializers/smtp_settings.rb
@@ -1,6 +1,6 @@
 ActionMailer::Base.smtp_settings = {
     :address => "smtp.gmail.com",
-    :port => 465,
+    :port => 587,
     :domain => "gmail.com",
     :user_name => "iam007dev@gmail.com",
     :password => "VikRam05",
